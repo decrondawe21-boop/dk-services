fbdbcef0f4e5d3135ee808e96d81123d7b5d6e94
decrondawe21-boop <decrondawe21@gmail.com>
2026-03-26 20:49:14 +0100

    Serve social images through static Vercel rewrites

--- api/og.tsx
+++
@@ -1,7 +0,0 @@
-import { createSocialImage } from './_lib/social-image';
-
-export const runtime = 'nodejs';
-
-export function GET(request: Request) {
-  return createSocialImage(request, 'og');
-}
